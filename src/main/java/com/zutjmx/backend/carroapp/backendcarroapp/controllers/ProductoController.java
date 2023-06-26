0ea36fa3570f0ae3b72da562946ae7911c515def
Jesus Zuñiga <jesuszt@hotmail.com>
2023-06-26 11:37:39 -0600

    Se coloca método generaDatos en el controlador de Productos

--- src/main/java/com/zutjmx/backend/carroapp/backendcarroapp/controllers/ProductoController.java
+++ src/main/java/com/zutjmx/backend/carroapp/backendcarroapp/controllers/ProductoController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.javafaker.Faker;
 import com.zutjmx.backend.carroapp.backendcarroapp.models.entities.Producto;
 import com.zutjmx.backend.carroapp.backendcarroapp.services.ProductoService;
 
@@ -22,5 +25,19 @@ public class ProductoController {
     @GetMapping("/productos")
     public ResponseEntity<List<Producto>> getAllProductos() {
         return new ResponseEntity<>(productoService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/genera/{cuantos}")
+    public String generaDatos(@PathVariable int cuantos) {
+        Faker faker = new Faker();
+        for (int i = 0; i < cuantos; i++) {
+            Producto producto = new Producto();
+            producto.setNombre(faker.commerce().productName());
+            producto.setDescripcion(faker.lorem().paragraph());
+            producto.setPrecio(faker.number().randomNumber(3, false));
+            //producto.setPrecio(Long.parseLong(faker.commerce().price()));
+            productoService.save(producto);
+        }
+        return "Ok";
     }
 }
--- src/main/java/com/zutjmx/backend/carroapp/backendcarroapp/services/ProductoServiceImpl.java
+++ src/main/java/com/zutjmx/backend/carroapp/backendcarroapp/services/ProductoServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.github.javafaker.Faker;
 import com.zutjmx.backend.carroapp.backendcarroapp.models.entities.Producto;
 import com.zutjmx.backend.carroapp.backendcarroapp.repositories.ProductoRepository;
 
@@ -31,8 +30,7 @@ public class ProductoServiceImpl implements ProductoService {
 
     @Override
     public void save(Producto producto) {
-        // TODO: Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        productoRepository.save(producto);
     }
 
     @Override
@@ -41,16 +39,4 @@ public class ProductoServiceImpl implements ProductoService {
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
 
-    @Transactional
-    public void generaDatos(Long cuantos) {
-        Faker faker = new Faker();
-        for (int i = 0; i < cuantos; i++) {
-            Producto producto = new Producto();
-            producto.setNombre(faker.commerce().productName());
-            producto.setDescripcion(faker.company().buzzword());
-            producto.setPrecio(faker.number().randomNumber(2, false));
-            productoRepository.save(producto);
-        }
-    }
-    
 }
