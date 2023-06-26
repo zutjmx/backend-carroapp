261d505218e797e4166a947f6fc07f6b093d3ce4
Jesus Zuñiga <jesuszt@hotmail.com>
2023-06-26 11:52:36 -0600

    Se agrega archivo import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos (nombre, descripcion, precio) VALUES ('Teclado Mecanico RGB', 'Teclado Mecanico con luces RGB switches cherry red', 1000);
+INSERT INTO productos (nombre, descripcion, precio) VALUES ('Samsung Smart TV 55', 'Color optimizado para una imagen vibrante y realista gracias al potente Crystal Processor 4K', 3000);
+INSERT INTO productos (nombre, descripcion, precio) VALUES ('Audifono Bluetooth Sony', 'Audifono Bluetooth para smartphone', 770);
+INSERT INTO productos (nombre, descripcion, precio) VALUES ('Memoria Corsair 16GB DDR5', 'Memoria RAM optimizada para Juegos', 3700);
+INSERT INTO productos (nombre, descripcion, precio) VALUES ('Asus Nvidia RTX 4080TI', 'Tarjeta Grafica para juegos en 4k', 5000); 
+INSERT INTO productos (nombre, descripcion, precio) VALUES ('CPU Intel Core i7-12700K', 'Alder Lake de 12 cores optimizados para multitareas', 5000);
